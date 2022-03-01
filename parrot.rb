6c1c0b45c72358768ca008a46b674e4fbc9d4df5
gianv322 <90941304+gianv322@users.noreply.github.com>
2022-03-01 14:55:52 -0500

    commit tree ab9305011164c04c2d784798964733d4109cb07e

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+default_phrase = "Squawk!"
+
+
+def parrot(default_phrase)
+    puts default_phrase
+    puts "Squawk!"
+end
+parrot(default_phrase)
